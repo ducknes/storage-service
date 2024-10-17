6fb22c8215755a5f849f12f5625fee37219eb4e0
i.antonov <i.antonov@smartway.today>
2024-10-17 13:09:47 +0300

    fixes

--- database/storage_repository.go
+++ database/storage_repository.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"encoding/json"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"os"
 	"storage-service/tools/storagecontext"
@@ -62,8 +63,13 @@ func (r *StorageRepositoryImpl) GetProducts(ctx storagecontext.StorageContext, l
 func (r *StorageRepositoryImpl) GetProduct(ctx storagecontext.StorageContext, productId string) (product Product, err error) {
 	collection := r.mongoClient.Database(r.database).Collection(r.collection)
 
+	objID, err := primitive.ObjectIDFromHex(productId)
+	if err != nil {
+		return product, err
+	}
+
 	filter := bson.M{
-		"_id": productId,
+		"_id": objID,
 	}
 
 	return product, collection.FindOne(ctx.Ctx(), filter).Decode(&product)
@@ -80,8 +86,13 @@ func (r *StorageRepositoryImpl) UpdateProducts(ctx storagecontext.StorageContext
 
 	operations := make([]mongo.WriteModel, 0, len(products))
 	for _, product := range products {
+		objID, err := primitive.ObjectIDFromHex(product.Id)
+		if err != nil {
+			return err
+		}
+
 		filter := bson.M{
-			"_id": product.Id,
+			"_id": objID,
 		}
 
 		update := bson.M{
@@ -99,13 +110,18 @@ func (r *StorageRepositoryImpl) UpdateProducts(ctx storagecontext.StorageContext
 func (r *StorageRepositoryImpl) DeleteProducts(ctx storagecontext.StorageContext, productIds []string) error {
 	collection := r.mongoClient.Database(r.database).Collection(r.collection)
 
+	objIds, err := toPrimitiveObjectIds(productIds)
+	if err != nil {
+		return err
+	}
+
 	filter := bson.M{
 		"_id": bson.M{
-			"$in": productIds,
+			"$in": objIds,
 		},
 	}
 
-	_, err := collection.DeleteMany(ctx.Ctx(), filter)
+	_, err = collection.DeleteMany(ctx.Ctx(), filter)
 	return err
 }
 
@@ -130,4 +146,16 @@ func toAny[T any](any []T) (result []any) {
 		result = append(result, v)
 	}
 	return result
+}
+
+func toPrimitiveObjectIds(ids []string) ([]primitive.ObjectID, error) {
+	objectIds := make([]primitive.ObjectID, 0, len(ids))
+	for _, id := range ids {
+		objectId, err := primitive.ObjectIDFromHex(id)
+		if err != nil {
+			return nil, err
+		}
+		objectIds = append(objectIds, objectId)
+	}
+	return objectIds, nil
 }
